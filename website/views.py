c55db536df26af5598454a0720839500a1c0af75
Aman Bhagat <abhagat>
2023-05-09 15:53:30 +0530

    code modifications

--- website/views.py
+++ website/views.py
@@ -1,19 +1,20 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Question
+from .models import Note
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('question')#Gets the note from the HTML 
+        note = request.form.get('note')#Gets the note from the HTML 
 
         if len(note) < 1:
-            flash('Add Question !!', category='error') 
+            flash('Note is too short!', category='error') 
         else:
             new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
             db.session.add(new_note) #adding the note to the database 
@@ -25,14 +26,12 @@ def home():
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
-    question = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    QuestionID = question['noteId']
-    question = question.query.get(QuestionID)
-    if question:
-        if question.user_id == current_user.id:
-            db.session.delete(question)
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
             db.session.commit()
 
     return jsonify({})
-
-        
